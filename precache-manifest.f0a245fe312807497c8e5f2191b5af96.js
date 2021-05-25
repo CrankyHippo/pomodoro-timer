42bb2dbc7cf629eee1158244ab7e25e089a9629b
Bryan Burnett <crankyhippo@gmail.com>
2021-05-25 13:03:08 -0700

    Add files via upload

---
+++ precache-manifest.f0a245fe312807497c8e5f2191b5af96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30da715d21c082f4e4b944e10c2a59ba",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "cb10eb75adcc2ef6fefe",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb10eb75adcc2ef6fefe",
+    "url": "/pomodoro-timer/static/js/main.637f859f.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
